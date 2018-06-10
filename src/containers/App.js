378182c517469591ccd874155d95e641c8be3f27
unknown <rahatulahsan@gmail.com>
2018-06-10 23:38:23 +0600

    Final App done with good folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -32,13 +32,14 @@ class App extends Component {
 	// 2nd calling render
 	render(){
 
-		const filteredRobots = this.state.robots.filter(robots=>{
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot=>{
 
-			return robots.name.toLowerCase()
-			.includes(this.state.searchfield.toLowerCase())
+			return robot.name.toLowerCase()
+			.includes(searchfield.toLowerCase())
 		})
 
-		if(this.state.robots.length === 0){
+		if(!robots.length){
 			return <h1>Loading</h1>
 		}else{
 
